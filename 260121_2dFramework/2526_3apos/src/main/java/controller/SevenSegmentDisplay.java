ee3a2fbb92ed926bab877c858deb4c718b0792af
theonlydano <pichler.dano@outlook.com>
2026-02-11 11:40:46 +0100

    add time display cl

---
+++ 260121_2dFramework/2526_3apos/src/main/java/controller/SevenSegmentDisplay.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.awt.*;
+
+public class SevenSegmentDisplay {
+    private Vector location;
+    private int height;
+    private int width;
+
+    public SevenSegmentDisplay(Vector location, int height, int width) {
+        this.location = location;
+        this.height = height;
+        this.width = width;
+    }
+
+    public SevenSegmentDisplay(Vector location) {
+        this.location = location;
+    }
+
+    public void setTime(int minutes, int seconds) {
+        
+    }
+
+    public void draw(Graphics g){
+
+    }
+}
